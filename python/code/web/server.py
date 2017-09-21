a0a1cb4d91295eb069f8155d767c69d01e3bbd79
yangshaoxiong <yangshaoxiong@boco.com.cn>
2017-09-21 18:28:33 +0800

    python learn
    1. mysql
    2. sqlite
    3. web

---
+++ python/code/web/server.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+# Web开发
+# server.py
+
+from wsgiref.simple_server import make_server
+
+from hello import application
+
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000...')
+
+httpd.serve_forever()
